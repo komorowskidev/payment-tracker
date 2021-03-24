50b29338281401584cf6d3776e118ea34c532cfa
komorowskidev <komorowskidev@gmail.com>
2021-03-24 21:06:25 +0100

    heroku dist bugfix

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/paymenttracker'));
+app.use(express.static('./dist/payment-tracker'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/paymenttracker/'}),
+    res.sendFile('index.html', {root: 'dist/payment-tracker/'}),
 );
 
 app.listen(process.env.PORT || 8080);
