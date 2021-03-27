f2358a6aef27c199f501e922b6374f88adfda34d
komorowskidev <komorowskidev@gmail.com>
2021-03-27 19:53:40 +0100

    login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') loginForm: NgForm;
   title = 'payment-tracker';
+
+  onSubmit() {
+    console.log(this.loginForm.value.email);
+    console.log(this.loginForm.value.password);
+  }
 }
